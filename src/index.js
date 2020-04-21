c3e6ccc63fb9081bfcfbde2d966d82c55aca1cc7
eito-fis <eamon@itofisher.com>
2020-04-20 21:31:17 -0400

    Removed react-redux-firebase and redux-firestore from index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,15 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
-import fbConfig from './config/fbConfig'
 
-const store = createStore(rootReducer,
-  compose(
-    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reactReduxFirebase(fbConfig), // redux binding for firebase
-    reduxFirestore(fbConfig) // redux bindings for firestore
-  )
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 serviceWorker.unregister()
