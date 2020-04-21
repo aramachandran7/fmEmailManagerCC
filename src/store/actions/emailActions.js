da414ab92d0ff53886874c79e5eafcf05bf27479
eito-fis <eamon@itofisher.com>
2020-04-20 21:39:57 -0400

    Removed react-redux-firebase and redux-firestore from add donation action

--- src/store/actions/emailActions.js
+++ src/store/actions/emailActions.js
@@ -1,34 +1,16 @@
-export const createDonation = (donation) => {
-    return (dispatch, getState, { getFirebase, getFirestore }) => {
-        // FOR TESTING
-        // console.log("shouldn't be undefined", donation)
-        // dispatch({type: 'CREATE_DONATION',donation:donation})
-        // make async call to db
+import db from '../../firebase.js'
 
-        const firestore = getFirestore();
-        firestore.collection('confirmedDonations').add({
+export const createDonation = (donation) => {
+    return ({dispatch, getState}) => {
+        db.collection("donations").add({
             PPEType:donation.PPEType,
             PPEquantity:donation.PPEquantity,
-            addedBy: 'AdiRamachandran',
             createdAt: new Date()
         }).then(()=> {
             dispatch({type: 'CREATE_DONATION',donation:donation})
         }).catch((err)=>{
             dispatch({type:'CREATE_DONATION_ERROR', err})
         })
-        //DELETE? 
-        // firestore.collection('Emails').delete({
-        //     Body:donation.Body, 
-        //     PPEType: donation.PPEType, 
-        //     PPEquantity: donation.PPEquantity,
-        //     Sender: donation.Sender, 
-        //     sendDate: donation.sendDate,
-        //     subject: donation.subject
-        // }).then(()=>{
-        //     dispatch({type: 'EMAIL_DELETED', donation:donation})
-        // }).catch((err)=>{
-        //     dispatch({type:'EMAIL_DELETED_ERROR', err})
-        // })
     }
 }
 
