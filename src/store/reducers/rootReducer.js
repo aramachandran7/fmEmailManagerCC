3f06906826f7723a18fdba70f1b3dba5347e2dec
eito-fis <eamon@itofisher.com>
2020-04-20 21:31:34 -0400

    Removed react-redux-firebase from root reducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,7 +1,6 @@
 import authReducer from'./authReducer'
 import emailReducer from './emailReducer'
 import { combineReducers } from 'redux'
-import { firestoreReducer } from 'redux-firestore'
 
 
 const initState = {}
@@ -9,7 +8,6 @@ const initState = {}
 const rootReducer = combineReducers({
     auth: authReducer,
     email: emailReducer,
-    firestore: firestoreReducer
 });
 
 export default rootReducer
