a8914e0be6c098d99a8d69f3d553c6923dac29cc
eito-fis <eamon@itofisher.com>
2020-04-22 13:33:13 -0400

    Removed create donation action

--- src/store/actions/emailActions.js
+++ src/store/actions/emailActions.js
@@ -1,20 +1,6 @@
 import db from '../../firebase.js'
 const COLLECTION = "Emails"
 
-export const createDonation = (donation) => {
-    return ({dispatch, getState}) => {
-        db.collection("donations").add({
-            PPEType:donation.PPEType,
-            PPEquantity:donation.PPEquantity,
-            createdAt: new Date()
-        }).then(()=> {
-            dispatch({type: 'CREATE_DONATION',donation:donation})
-        }).catch((err)=>{
-            dispatch({type:'CREATE_DONATION_ERROR', err})
-        })
-    }
-}
-
 export const setStatus = (email, status) => {
     return ((dispatch, getState) => {
         db.collection(COLLECTION).doc(email.id)
